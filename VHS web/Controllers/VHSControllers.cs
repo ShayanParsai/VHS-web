de4b0d133a8c8c078aea6cd2189d8b109777060a
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2021-12-08 11:15:08 +0100

    Add files via upload

---
+++ VHS web/Controllers/VHSControllers.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VHS_web.Controllers
+{
+    [Route("api/VHS")]
+    [ApiController]
+    public class VHSControllers : ControllerBase
+    {
+        [HttpGet]
+        [Route("/Status")]
+        public void get_status()
+        {
+            
+        }
+
+        [HttpPost]
+        [Route("/Status")]
+        public void post_status()
+        {
+            
+        }
+
+        [HttpPost]
+        [Route("/PostAlarm")]
+
+        [HttpPost]
+        [Route("/GetPosition")]
+
+        [HttpGet]
+        [Route("/Auth")]
+
+        [HttpGet]
+        [Route("/GetCustomer")]
+
+        [HttpGet]
+        [Route("/Validate/")]
+
+    }       
+
+}
